db3dd8b7596d1def9da2672ee21ae43abd2438cf
Joanna Szefer <jo.szefer@gmail.com>
2021-02-22 17:26:26 +0100

    Add style to items

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -9,23 +9,58 @@ const ItemsWrapper = styled.div`
   flex-wrap: wrap;
   flex-direction: row;
   justify-content: space-evenly;
+  @media (max-width: 700px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 const StyledItem = styled.div`
   background-color: white;
   margin: 10px;
-  width: 30vw;
-  height: auto;
+  padding: 20px;
+  width: 20vw;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   img {
-    width: 70%;
+    width: 100%;
     height: auto;
+    background-size: cover;
   }
   span {
     background-color: white;
-    padding: 10px;
+    padding: 15px;
+    font-size: 22px;
+    font-family: sans-serif;
+    font-weight: bold;
   }
+  .price {
+    color: rgb(36, 196, 134);
+    font-size: 20px;
+  }
+  @media (max-width: 700px) {
+    img {
+      width: 60vw;
+    }
+  }
+`;
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledButton = styled.button`
+  font-size: 16px;
+  cursor: pointer;
+  margin: 5px;
+  padding: 10px 12px;
+  border-radius: 3px;
+  text-align: center;
+  color: white;
+  border: none;
+  font-weight: bold;
+  background: linear-gradient(rgb(46, 204, 113), rgb(26, 188, 156));
 `;
 
 export const Items = () => {
