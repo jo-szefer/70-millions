37b7e16f7a81f4960dd17de66a878ea08e47b694
Joanna Szefer <jo.szefer@gmail.com>
2021-02-22 17:09:20 +0100

    add new products to the shop

--- src/components/Items/itemsList.js
+++ src/components/Items/itemsList.js
@@ -4,12 +4,24 @@ import respiratorJPG from "../../images/respirator.jpg";
 import teslaJPG from "../../images/tesla.jpg";
 import vermeerJPG from "../../images/vermeer.jpg";
 import netflixJPG from "../../images/netflix.jpg";
+import dronJPG from "../../images/dron.jpg";
+import kapcieJPG from "../../images/kapcie.jpg";
+import konJPG from "../../images/koń.jpg";
+import whiteHouseJPG from "../../images/white_house.jpg";
+import bibliaJPG from "../../images/ksiazka.jpg";
+import helikopterJPG from "../../images/helikopter.jpg";
 
 export const itemsList = [
-  { name: "Kanapka drwala", image: drwalJPG, price: 10 },
-  { name: "Klapki Kubota", image: kubotyJPG, price: 10 },
-  { name: "Respirator", image: respiratorJPG, price: 1000 },
-  { name: "Tesla", image: teslaJPG, price: 10 },
-  { name: "Obraz", image: vermeerJPG, price: 110 },
-  { name: "Abonament Netflix", image: netflixJPG, price: 10 },
+  { name: "Kanapka drwala", image: drwalJPG, price: 30 },
+  { name: "Klapki Kubota", image: kubotyJPG, price: 25 },
+  { name: "Respirator", image: respiratorJPG, price: 82000 },
+  { name: "Tesla", image: teslaJPG, price: 440000 },
+  { name: "Obraz", image: vermeerJPG, price: 68000000 },
+  { name: "Abonament Netflix", image: netflixJPG, price: 30 },
+  { name: "Dron", image: dronJPG, price: 460 },
+  { name: "Cieplutkie bambosze", image: kapcieJPG, price: 15 },
+  { name: "Koń", image: konJPG, price: 30000 },
+  { name: "Ładny dom", image: whiteHouseJPG, price: 70000000 },
+  { name: "Niezła książka", image: bibliaJPG, price: 60 },
+  { name: "Helikopter", image: helikopterJPG, price: 400000 },
 ];
