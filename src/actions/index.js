e9fe8a4627a47c8ef41b7f28a3b5850f28c19473
Joanna Szefer <jo.szefer@gmail.com>
2021-02-22 17:39:01 +0100

    Add redux actions and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const addProduct = (product) => ({
+  type: "ADD_PRODUCT",
+  product,
+});
+
+export const sellProduct = (product) => ({
+  type: "SELL_PRODUCT",
+  product,
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,52 @@
+const initialState = {
+  money: 70000000,
+  products: [],
+};
+
+export const reducer = (state = initialState, action) => {
+  const index = action.product
+    ? getIndexOfProduct(state.products, action.product.name)
+    : -1;
+
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return {
+        ...state,
+        money: state.money - action.product.price,
+        products:
+          index === -1
+            ? [...state.products, { ...action.product, quantity: 1 }]
+            : increaseQuantity(state.products, index),
+      };
+    case "SELL_PRODUCT":
+      return {
+        ...state,
+        money: index === -1 ? state.money : state.money + action.product.price,
+        products:
+          index === -1
+            ? state.products
+            : decreaseQuantity(state.products, index),
+      };
+    default:
+      return state;
+  }
+};
+
+const getIndexOfProduct = (products, name) => {
+  const index = products.findIndex((product) => product.name === name);
+  return index;
+};
+const increaseQuantity = (products, index) => {
+  const newProducts = [...products];
+  newProducts[index].quantity++;
+  return newProducts;
+};
+const decreaseQuantity = (products, index) => {
+  const newProducts = [...products];
+  if (newProducts[index].quantity === 1) {
+    newProducts.splice(index, 1);
+  } else {
+    newProducts[index].quantity--;
+  }
+  return newProducts;
+};
