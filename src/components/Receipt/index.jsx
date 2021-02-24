f9a35f8e621e734d8e5e318245ece5bcf6a1896b
Joanna Szefer <jo.szefer@gmail.com>
2021-02-24 12:42:22 +0100

    Add styles &  formatPrice function

--- src/components/Receipt/index.jsx
+++ src/components/Receipt/index.jsx
@@ -2,38 +2,71 @@ import React from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 
-const WrappedReceipt = styled.div`
-  // background-color: #c6b9cd;
+const ReceiptContainer = styled.div`
   box-shadow: 4px 4px 14px 0px rgba(50, 50, 50, 0.75);
-  width: 30vw;
-  height: 40vh;
+  width: fit-content;
   display: flex;
-  margin: 0 auto;
-  justify-content: center;
+  flex-direction: column;
+  margin: 10px auto;
+  align-items: center;
   font-family: Roboto, sans-serif;
-  h1 {
+  padding: 20px;
+`;
+const ProductsContainer = styled.div`
+  display: grid;
+  gap: 10px 0px;
+`;
+
+const Product = styled.div`
+  text-align: center;
+  display: grid;
+  grid-template-columns: 200px 45px 200px;
+  @media (max-width: 500px) {
+    grid-template-columns: 100px 45px 100px;
   }
 `;
-const Item = styled.span``;
-const Product = styled.div``;
-const Quantity = styled.span``;
-const Price = styled.span``;
+const Total = styled.span`
+  width: 90%;
+  margin: 10px;
+  padding: 20px;
+  border-top: solid 2px;
+  font-family: Roboto, sans-serif;
+  font-weight: bold;
+  text-align: center;
+`;
 
 const _Receipt = (props) => {
-  return (
-    <WrappedReceipt>
-      <h1>Rachunek</h1>
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+  const totalPrice = (products) => {
+    const total = products.reduce(
+      (sum, product) => sum + product.price * product.quantity,
+      0
+    );
+    return formatPrice(total);
+  };
 
-      {props.products
-        ? props.products.map((product) => (
-            <Product>
-              <Item>{product.name}</Item>
-              <Quantity>{product.quantity}</Quantity>
-              <Price>{product.price}</Price>
-            </Product>
-          ))
-        : null}
-    </WrappedReceipt>
+  return (
+    <>
+      {props.products.length ? (
+        <>
+          <ReceiptContainer>
+            <h1>Rachunek</h1>
+            <ProductsContainer>
+              {props.products.map((product) => (
+                <Product>
+                  <span>{product.name}</span>
+                  <span>X{product.quantity}</span>
+                  <span>{formatPrice(product.price)} zł</span>
+                </Product>
+              ))}
+            </ProductsContainer>
+            <Total>Razem: {totalPrice(props.products)} zł</Total>
+          </ReceiptContainer>
+        </>
+      ) : null}
+    </>
   );
 };
 
